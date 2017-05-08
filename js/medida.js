32fa775c5ba3b88f703225b1c00a26df7e5202c6
Dani <alu0100886764@ull.edu.es>
2017-05-08 18:45:46 +0000

    Partiendo del repositorio switch smell

---
+++ js/medida.js
@@ -0,0 +1,82 @@
+/*global XRegExp*/
+(function(exports){
+  var regexp = '^(\\s*)                                       ' +
+                '(?<valor> [-+]?[0-9]+(?:\\.[0-9]+)?) ' +
+                '(\\s*)                                               ' +
+                '(?<tipo> [\\/\\-\\*\\+])                              ' +
+                '(\\s*)';
+
+  function Medida(valor,tipo)
+  {
+    console.log("Accedo a clase Medida");
+    if(tipo)
+    {
+      this.valor = valor || 0;
+      this.tipo  = tipo;
+    }
+    else
+    {
+      var expresion;
+      console.log("RegExp:"+regexp);
+      expresion = XRegExp.exec(valor,XRegExp(regexp,'ix'));
+      console.log("Expresion:"+expresion);
+      if(expresion)
+      {
+        var numero = expresion.valor;
+        numero = parseFloat(numero);
+        var tipo = expresion.tipo;
+        tipo = tipo.toLowerCase();
+        this.valor = numero;
+        this.tipo = tipo;
+        console.log("Valor: " + this.valor + ", Tipo: " + this.tipo);
+      }
+    }
+  }
+  Medida.constructor = Medida;
+  Medida.measures = Medida.measures || {};
+
+  Medida.match = function(valor)
+  {
+    var exp_regular = '(\\s*)'+
+                      '(?<to> [-+]?[0-9]+(?:\\.[0-9]+)?)'+
+                      '(\\s*)$';
+
+    var res = XRegExp.exec(valor,XRegExp(regexp.concat(exp_regular),'ix'));
+    console.log("Numero:"+res.valor+", tipo: "+res.tipo+", Destino:"+res.to);
+    return res;
+  }
+
+  Medida.convertir = function(valor) {
+    var measures = Medida.measures;
+    var match = Medida.match(valor);
+    if (match) {
+      var numero = match.valor,
+          tipo   = match.tipo,
+          destino = match.to;
+      tipo = tipo.toLowerCase();
+      destino = destino.toLowerCase();
+      console.log("Numero:"+numero+",Tipo:"+tipo+",Destino:"+destino);
+      try {
+        var source = new measures[tipo](numero);  // new Fahrenheit(32)
+        console.log("Source:"+source);
+        var target = "to"+measures[destino].name; // "toCelsius"
+        console.log("Target:"+target);
+        console.log("Return:"+source[target]().valor);
+        return source[target]().valor + " "+measures[destino].name; // "0 Celsius"
+      }
+      catch(err) {
+        if (tipo == destino){
+          return numero + " " + destino;
+        }
+        else{
+          return 'Desconozco como convertir desde "'+tipo+'" hasta "'+destino+'"';
+        }
+      }
+    }
+    else
+      return "Introduzca una temperatura valida: 330e-1 F to C";
+  };
+
+  exports.Medida = Medida;
+
+})(this);
---
+++ js/temperature.js
@@ -0,0 +1,84 @@
+(function(){
+  
+    var measures = Medida.measures || {};
+  
+    function Temperatura(valor,tipo)
+    {
+      console.log("Accedo a clase Temperatura");
+      Medida.call(this,valor,tipo);
+      /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    }
+    Temperatura.prototype = new Medida();
+    Temperatura.prototype.constructor = Temperatura;
+
+  // ----------------------------------------------------- //
+
+    function Celsius(valor)
+    {
+      var c_tof = 0;
+      var c_tok = 0;
+      console.log("Accedo a clase Celsius");
+      Temperatura.call(this,valor,'c');
+    }
+    Celsius.prototype = new Temperatura;
+    Celsius.prototype.constructor = Celsius;
+    measures.c = Celsius;
+    Celsius.prototype.toFarenheit = function()
+    {
+      var c_tof = (this.valor * 9/5) + 32;
+      return new Farenheit(c_tof);
+    }
+    Celsius.prototype.toKelvin = function()
+    {
+      var f_toK = (this.valor + 273.15);
+      return new Kelvin(f_toK);
+    }
+    
+    
+  // ----------------------------------------------------- //
+  
+    function Farenheit(valor)
+    {
+      var f_toC = 0;
+      var f_toK = 0;
+      console.log("Accedo a la clase Fahrenheit.");
+      Temperatura.call(this,valor,'f');
+    }
+    Farenheit.prototype = new Temperatura;
+    Farenheit.prototype.constructor = Farenheit;
+    measures.f = Farenheit;
+    Farenheit.prototype.toCelsius = function()
+    {
+        var f_toC = (this.valor - 32) * 5/9;
+        return new Celsius(f_toC);
+    }
+    Farenheit.prototype.toKelvin = function()
+    {
+      var f_toK = (this.toCelsius().valor + 273.15);
+      return new Kelvin(f_toK);
+    }
+
+  // ----------------------------------------------------- //
+  
+    function Kelvin(valor)
+    {
+      var k_toC = 0;
+      var k_toF = 0;
+      console.log("Accedo a clase Kelvin");
+      Temperatura.call(this,valor,'k');
+    }
+    Kelvin.prototype = new Temperatura;
+    Kelvin.prototype.constructor = Kelvin;
+    measures.k = Kelvin;
+    Kelvin.prototype.toCelsius = function()
+    {
+      var k_toC = (this.valor - 273.15);
+      return new Celsius(k_toC);
+    }
+    Kelvin.prototype.toFarenheit = function()
+    {
+      var k_toF = (this.toCelsius().valor * 9/5) + 32;
+      return new Farenheit(k_toF);
+    }
+
+})(this);
